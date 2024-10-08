1aabfdb1c3ebe144fa3d477280da10006d1e7eb1
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-07 21:09:08 -0600

    Actualizar Random.py

--- Random.py
+++ Random.py
@@ -2,3 +2,10 @@
 # Fecha: 11 Septiembre 2024
 # Descripción: Código para procesamiento de imagenes con Sobel.
 # GitHub: https://github.com/Jona163
+
+## importaciones a requerir 
+import turtle
+import pygame
+import time
+import numpy as np
+import matplotlib.pyplot as plt
